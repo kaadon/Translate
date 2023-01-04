692bc34018e6b5649851163aa9272d04622cb297
kaadon.com <kaadon.com@gmail.com>
2023-01-04 18:42:04 +0800

    first commit 🐴:

---
+++ b.php
@@ -0,0 +1,20 @@
+<?php
+require 'vendor/autoload.php';
+
+use Google\Cloud\Translate\V3\TranslationServiceClient;
+
+$translationClient = new TranslationServiceClient();
+$content = ['one', 'two', 'three'];
+$targetLanguage = 'es';
+$response = $translationClient->translateText(
+    $content,
+    $targetLanguage,
+    TranslationServiceClient::locationName('translation-330800', 'global')
+);
+
+foreach ($response->getTranslations() as $key => $translation) {
+    $separator = $key === 2
+        ? '!'
+        : ', ';
+    echo $translation->getTranslatedText() . $separator;
+}
